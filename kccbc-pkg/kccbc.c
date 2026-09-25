287898197c7dd459e8400c7879d9f01b25d402b4
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-25 11:18:12 -0500

    Update kccbc.c

--- kccbc-pkg/kccbc.c
+++ kccbc-pkg/kccbc.c
@@ -7,6 +7,47 @@
 
 #define KCCBC 1
 
+struct kccbc {
+  /*accumulator*/
+  int al;
+  int ah;
+  int ax;
+  int eax;
+  int rax;
+  /*base*/
+  int bl;
+  int bh;
+  int bx;
+  int ebx;
+  int rbx;
+  /*counter*/
+  int cl;
+  int ch;
+  int cx;
+  int ecx;
+  int rcx;
+  /*data*/
+  int dl;
+  int dh;
+  int dx;
+  int edx;
+  int rdx;
+  /*stack pointer*/
+  int sp;
+  int esp;
+  /*push*/
+  int push;
+  int pop;
+  /*oper*/
+  int add;
+  int sub;
+  int mul;
+  int div;
+  /*mov*/
+  void* mov;
+
+};
+
 /*execute_kccbc_command()*/
 void __stdcall execute_kccbc_command( void* EXEC );
 
