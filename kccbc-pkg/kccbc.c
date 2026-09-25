cb856132ad7b6d84a9ddf07332d9e390bc11abe1
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-25 11:12:01 -0500

    Create kccbc.c

---
+++ kccbc-pkg/kccbc.c
@@ -0,0 +1,16 @@
+/*kccbc.c*/
+/*
+  - KCCBC.
+  - Sept. 2026.
+  - @Copyrighted.
+*/
+
+#define KCCBC 1
+
+/*execute_kccbc_command()*/
+void __stdcall execute_kccbc_command( void* EXEC );
+
+/*main()*/
+int main() { 
+  return 0;
+}
